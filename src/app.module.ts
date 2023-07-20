9cd69f813da4ef842030ef60befb7187ce96b4f1
AbdulrahmanKanaan <abdulrahman.kanaan44@gmail.com>
2023-07-20 06:35:59 +0300

    clean up

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 import { ConfigModule } from '@nestjs/config';
+import { CoreModule } from './core/core.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot(), CoreModule],
 })
 export class AppModule {}
