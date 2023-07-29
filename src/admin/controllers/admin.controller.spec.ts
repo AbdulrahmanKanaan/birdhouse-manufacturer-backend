05128bb2327c51513024de9f2e6c6c4e78b02f5c
AbdulrahmanKanaan <abdulrahman.kanaan44@gmail.com>
2023-07-29 14:36:11 +0300

    controllers

--- src/admin/controllers/admin.controller.spec.ts
+++ src/admin/controllers/admin.controller.spec.ts
@@ -1,18 +1,95 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AdminController } from './admin.controller';
+import { AdminService } from '../services';
+import { HouseService } from '&/core/services';
+import { v4 } from 'uuid';
+import { Birdhouse, Residency } from '&/domain/entities';
+import { Page } from '&/common/types';
+import { ListHousesDto } from '../dto';
 
 describe('AdminController', () => {
   let controller: AdminController;
+  let adminService: AdminService;
+  let houseService: HouseService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AdminController],
+      providers: [
+        {
+          provide: AdminService,
+          useValue: {
+            listBirdhouses: jest.fn(),
+            getHistory: jest.fn(),
+          },
+        },
+        { provide: HouseService, useValue: { getBirdhouse: jest.fn() } },
+      ],
     }).compile();
 
     controller = module.get<AdminController>(AdminController);
+    adminService = module.get<AdminService>(AdminService);
+    houseService = module.get<HouseService>(HouseService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('listHouses', () => {
+    it('should return a page of birdhouses', async () => {
+      const birdhouse = new Birdhouse(v4(), v4(), 'test', 12.34, 56.78, null);
+
+      const dto: ListHousesDto = {
+        page: 1,
+        perPage: 10,
+      };
+
+      const page = new Page([birdhouse], 1, dto.page, dto.perPage);
+
+      jest.spyOn(adminService, 'listBirdhouses').mockResolvedValue(page);
+
+      const result = await controller.listHouses(dto);
+
+      expect(adminService.listBirdhouses).toHaveBeenCalled();
+      expect(adminService.listBirdhouses).toHaveBeenCalledWith(dto);
+      expect(result).toEqual(page);
+    });
+  });
+
+  describe('getBirdhouse', () => {
+    it('should return a birdhouse', async () => {
+      const birdhouse = new Birdhouse(v4(), v4(), 'test', 12.34, 56.78, null);
+
+      jest.spyOn(houseService, 'getBirdhouse').mockResolvedValue(birdhouse);
+
+      const result = await controller.getBirdhouse(birdhouse.id!);
+
+      expect(houseService.getBirdhouse).toHaveBeenCalled();
+      expect(houseService.getBirdhouse).toHaveBeenCalledWith(birdhouse.id);
+      expect(result).toEqual(birdhouse);
+    });
+  });
+
+  describe('getHistory', () => {
+    it('should return a page of residencies', async () => {
+      const birdhouse = new Birdhouse(v4(), v4(), 'test', 12.34, 56.78, null);
+      const residency = new Residency(1, birdhouse.id!, 2, 3);
+
+      const dto: ListHousesDto = {
+        page: 1,
+        perPage: 10,
+      };
+
+      const page = new Page<Residency>([residency], 1, dto.page, dto.perPage);
+
+      jest.spyOn(adminService, 'getHistory').mockResolvedValue(page);
+
+      const result = await controller.getHistory(birdhouse.id!, dto);
+
+      expect(adminService.getHistory).toHaveBeenCalled();
+      expect(adminService.getHistory).toHaveBeenCalledWith(birdhouse.id, dto);
+      expect(result).toEqual(page);
+    });
   });
 });
--- src/bird/controllers/bird.controller.spec.ts
+++ src/bird/controllers/bird.controller.spec.ts
@@ -1,18 +1,133 @@
+import { HouseService } from '&/core/services';
+import { Birdhouse } from '&/domain/entities';
 import { Test, TestingModule } from '@nestjs/testing';
+import { v4 } from 'uuid';
+import { AuthGuard } from '../auth';
+import { RegisterDto, UpdateHouseDto } from '../dto';
+import { AddResidencyDto } from '../dto/add-residency.dto';
+import { BirdService } from '../services';
 import { BirdController } from './bird.controller';
 
 describe('BirdController', () => {
   let controller: BirdController;
 
+  let birdService: BirdService;
+  let houseService: HouseService;
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [BirdController],
-    }).compile();
+      providers: [
+        {
+          provide: BirdService,
+          useValue: {
+            createBirdhouse: jest.fn(),
+            updateBirdhouse: jest.fn(),
+            addResidency: jest.fn(),
+          },
+        },
+        { provide: HouseService, useValue: { getBirdhouse: jest.fn() } },
+      ],
+    })
+      .overrideGuard(AuthGuard)
+      .useValue({})
+      .compile();
 
     controller = module.get<BirdController>(BirdController);
+    birdService = module.get<BirdService>(BirdService);
+    houseService = module.get<HouseService>(HouseService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('createBirdhouse', () => {
+    it('should create a birdhouse and return it', async () => {
+      const registerDto: RegisterDto = {
+        name: 'Test name',
+        latitude: 37.7749,
+        longitude: -122.4194,
+      };
+      const birdhouse = new Birdhouse(v4(), v4(), 'test', 12.34, 56.78, null);
+      jest
+        .spyOn(birdService, 'createBirdhouse')
+        .mockResolvedValueOnce(birdhouse);
+
+      const result = await controller.createBirdhouse(registerDto);
+
+      expect(birdService.createBirdhouse).toHaveBeenCalled();
+      expect(birdService.createBirdhouse).toHaveBeenCalledWith(registerDto);
+      expect(result).toBeInstanceOf(Birdhouse);
+      expect(result).toEqual(birdhouse);
+    });
+  });
+
+  describe('getBirdhouse', () => {
+    it('should get a birdhouse by id and return it', async () => {
+      const birdhouse = new Birdhouse(v4(), v4(), 'test', 12.34, 56.78, null);
+
+      jest.spyOn(houseService, 'getBirdhouse').mockResolvedValueOnce(birdhouse);
+
+      const result = await controller.getBirdhouse(birdhouse.id!);
+
+      expect(houseService.getBirdhouse).toHaveBeenCalled();
+      expect(houseService.getBirdhouse).toHaveBeenCalledWith(birdhouse.id!);
+      expect(result).toBeInstanceOf(Birdhouse);
+      expect(result).toEqual(birdhouse);
+    });
+  });
+
+  describe('updateBirdhouse', () => {
+    it('should update a birdhouse and return it', async () => {
+      const updateHouseDto: UpdateHouseDto = {
+        name: 'new name',
+        latitude: 37.7749,
+        longitude: -122.4194,
+      };
+      const birdhouse = new Birdhouse(v4(), v4(), 'test', 12.34, 56.78, null);
+
+      jest
+        .spyOn(birdService, 'updateBirdhouse')
+        .mockResolvedValueOnce(birdhouse);
+
+      const result = await controller.updateBirdhouse(
+        birdhouse.id!,
+        updateHouseDto,
+      );
+
+      expect(birdService.updateBirdhouse).toHaveBeenCalled();
+      expect(birdService.updateBirdhouse).toHaveBeenCalledWith(
+        birdhouse.id!,
+        updateHouseDto,
+      );
+      expect(result).toBeInstanceOf(Birdhouse);
+      expect(result).toEqual(birdhouse);
+    });
+  });
+
+  describe('addResidency', () => {
+    it('should add a residency to a birdhouse and return it', async () => {
+      const addResidencyDto: AddResidencyDto = {
+        eggs: 1,
+        birds: 2,
+      };
+      const birdhouse = new Birdhouse(v4(), v4(), 'test', 12.34, 56.78, null);
+
+      jest.spyOn(birdService, 'addResidency').mockResolvedValueOnce(birdhouse);
+
+      const result = await controller.addResidency(
+        birdhouse.id!,
+        addResidencyDto,
+      );
+
+      expect(birdService.addResidency).toHaveBeenCalled();
+      expect(birdService.addResidency).toHaveBeenCalledWith(
+        birdhouse.id!,
+        addResidencyDto,
+      );
+      expect(result).toBeInstanceOf(Birdhouse);
+      expect(result).toEqual(birdhouse);
+    });
   });
 });
